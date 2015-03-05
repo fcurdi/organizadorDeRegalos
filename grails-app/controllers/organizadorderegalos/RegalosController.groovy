64f643df39b68bc7f473922b84647e9b56b9c711
Eric Brandwein <eric.brandwein@mercadolibre.com>
2015-03-05 14:06:49 -0300

    Agregado boton de guardar el producto y preparado el controller para cuando se pueda elegir el producto.

--- grails-app/controllers/organizadorderegalos/RegalosController.groovy
+++ grails-app/controllers/organizadorderegalos/RegalosController.groovy
@@ -8,4 +8,12 @@ class RegalosController {
 		[empleados:Empleado.list()]
 	}
 	
+	def guardarRegalo(){
+		Calendar c =(new Date()).toCalendar();
+		Regalo nuevo=new Regalo(titulo:params.titulo,url:params.url,
+			thumbnail:params.thumbnail,fechaAgregado:c);
+		nuevo.addToEmpleado(params.idEmpleado);
+		nuevo.save();
+	}
+	
 }
