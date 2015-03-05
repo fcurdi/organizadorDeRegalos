47e8aa71f5c70a8c388c1c7c109e2ae61437eb7f
Eric Brandwein <eric.brandwein@mercadolibre.com>
2015-03-05 15:12:39 -0300

    Casi arreglado el action guardarRegalo del RegalosController.

--- grails-app/controllers/organizadorderegalos/EmpleadosController.groovy
+++ grails-app/controllers/organizadorderegalos/EmpleadosController.groovy
@@ -19,4 +19,8 @@ class EmpleadosController {
 			 dni:params.dni,fechaNacimiento:c);
 		 nuevo.save();
 	}
+	
+	def editarEmpleado(){
+		
+	}
 }
--- grails-app/controllers/organizadorderegalos/RegalosController.groovy
+++ grails-app/controllers/organizadorderegalos/RegalosController.groovy
@@ -12,8 +12,10 @@ class RegalosController {
 		Calendar c =(new Date()).toCalendar();
 		Regalo nuevo=new Regalo(titulo:params.titulo,url:params.url,
 			thumbnail:params.thumbnail,fechaAgregado:c);
-		nuevo.addToEmpleado(params.idEmpleado);
+		nuevo.addToEmpleado(Empleado.get(params.idEmpleado));
 		nuevo.save();
+		
+		redirect(controller:"index",action:"index");
 	}
 	
 }
