1eb9266c9e01e2413df9dc975b8288022322d2f3
Eric Brandwein <eric.brandwein@mercadolibre.com>
2015-03-05 11:01:11 -0300

    Arreglado el guardado de un empleado y agregado un datepicker para la fecha de nacimiento.

--- grails-app/controllers/organizadorderegalos/EmpleadosController.groovy
+++ grails-app/controllers/organizadorderegalos/EmpleadosController.groovy
@@ -15,8 +15,12 @@ class EmpleadosController {
 	}
 	
 	def guardarEmpleado(){
+		Calendar c = Calendar.getInstance();
+		def aniomesdia=params.fecha.split("-").collect{Integer.parseInt(it)};
+		c.set(aniomesdia[0],aniomesdia[1],aniomesdia[2]);
+		
 		 Empleado nuevo=new Empleado(nombre:params.nombre,apellido:params.apellido,
-			 dni:params.dni,fechaNacimiento:params.fecha);
+			 dni:params.dni,fechaNacimiento:c);
 		 nuevo.save();
 	}
 }
