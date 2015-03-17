a3931a848dda9d4724e43ff20039e1605ed869fc
fcurdi <franciscocurdi@gmail.com>
2015-03-17 12:05:58 -0300

    hardcodeo de empresa en el boostrap y belongsTo de user a empresa

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -2,17 +2,24 @@ import organizadorderegalos.Empleado
 import com.testapp.Role
 import com.testapp.User
 import com.testapp.UserRole
+import organizadorderegalos.Empresa
 
 class BootStrap {
 	
     def init = { servletContext ->
-		def adminRole = new Role(authority: 'ROLE_ADMIN').save(flush: true)
+
+		def empresaRoot = new Empresa(razon_social: "ROOT SRL",cuit: 000000000);
+		empresaRoot.save(flush: true,failOnError: true);
+
+		def adminRole = new Role(authority: 'ROLE_ADMIN').save(flush: true,failOnError: true);
 		def userRole = new Role(authority: 'ROLE_USER').save(flush: true)
+		def adminUser = new User(username: 'root', password: 'Meli2014');
 
-		def adminUser = new User(username: 'root', password: 'Meli2014')
-		adminUser.save(flush: true)
-
-		UserRole.create(adminUser, adminRole, true)
+		empresaRoot.addToAdmins(adminUser);
+		empresaRoot.save(flush: true,failOnError: true);
+		
+		adminUser.save(flush: true,failOnError: true);
+		UserRole.create(adminUser, adminRole, true);
     }
 	
     def destroy = {
